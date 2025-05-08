6104ccdeda93486b2b2dbc842046e982af8e77d7
Komsit Prakobphol <pkomsit@folio-sec.com>
2025-05-08 12:40:51 +0900

    Add keymaps for terminal and code comments

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,9 +6,19 @@ local wk = require("which-key")
 -- Register terminal group with Nerd Font icon
 wk.add({ "<leader>t", group = "Terminal" })
 
+-- Terminal keymaps
 vim.keymap.set("n", "<leader>tt", function()
   Snacks.terminal.toggle()
 end, { desc = "Toggle Snacks Terminal" })
 vim.keymap.set("n", "<leader>tf", function()
   Snacks.terminal.toggle(nil, { win = { style = "float" } })
 end, { desc = "Toggle Snacks Terminal (Float)" })
+
+-- Disable defalt terminal keymaps
+vim.keymap.del("n", "<leader>ft")
+vim.keymap.del("n", "<leader>fT")
+
+-- Code
+-- Comments
+vim.keymap.set("n", "<leader>c/", "gcc", { remap = true, desc = "Toggle comment line (gcc)" })
+vim.keymap.set("v", "<leader>c/", "gc", { remap = true, desc = "Toggle comment (gc)" })
