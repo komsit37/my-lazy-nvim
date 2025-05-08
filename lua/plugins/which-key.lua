c928a12f69199e8db2045df98ec070797458faf3
Komsit Prakobphol <pkomsit@folio-sec.com>
2025-05-08 12:41:26 +0900

    change which-key preset

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,14 @@
+if true then
+  return {
+    {
+      "folke/which-key.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        preset = "modern",
+      },
+    },
+  }
+end
