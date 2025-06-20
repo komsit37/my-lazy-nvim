40f671eab63422ef6c4e80ac9061269cb3abef81
Komsit Prakobphol <pkomsit@folio-sec.com>
2025-06-21 06:59:13 +0900

    add markdown lint rule

---
+++ lua/plugins/lint.lua
@@ -0,0 +1,13 @@
+-- ~/.config/nvim/lua/plugins/lint.lua
+local HOME = os.getenv("HOME")
+return {
+  "mfussenegger/nvim-lint",
+  optional = true,
+  opts = {
+    linters = {
+      ["markdownlint-cli2"] = {
+        args = { "--config", HOME .. "/.config/nvim/markdownlint-cli2.yaml", "--" },
+      },
+    },
+  },
+}
