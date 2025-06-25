09ca128afe542a89dc28c2173ef321939d290fd6
Komsit Prakobphol <pkomsit@folio-sec.com>
2025-06-25 15:22:49 +0900

    fix occasional markview loading warning

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,8 +1,13 @@
 -- For `plugins/markview.lua` users.
 return {
   "OXY2DEV/markview.nvim",
-  lazy = false,
+  lazy = true,
 
+  -- to fix warning nvim-treesitter  is being loaded before  markview.nvim
+  priority = 1000, -- Higher priority than treesitter
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
   -- For blink.cmp's completion
   -- source
   -- dependencies = {
