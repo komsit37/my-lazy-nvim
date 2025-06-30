526a26880fce513a2532fc2d7ce1e3e78c943d4f
Komsit Prakobphol <pkomsit@folio-sec.com>
2025-06-30 12:48:51 +0900

    add sound

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -18,4 +18,38 @@ vim.g.clipboard = {
     end,
   },
 }
-vim.o.clipboard = "unnamedplus"
+
+-- Remove or comment out this line to disable auto sync:
+-- vim.o.clipboard = "unnamedplus"
+
+-- To yank to system clipboard explicitly, use the + register:
+--   "+yy   - yank current line to system clipboard
+--   "+y    - yank visual selection to system clipboard
+--
+-- Since clipboard = "unnamedplus" is disabled,
+-- Neovim won't auto-sync yanks to system clipboard,
+-- so you must use "+ before yank commands to copy externally.
+
+-- Define system sounds for each mode (macOS built-in)
+local mode_sounds = {
+  n = "/System/Library/Sounds/Glass.aiff", -- Normal mode
+  i = "/System/Library/Sounds/Hero.aiff", -- Insert mode
+  v = "/System/Library/Sounds/Bottle.aiff", -- Visual mode
+  V = "/System/Library/Sounds/Bottle.aiff", -- Visual Line
+  ["\22"] = "/System/Library/Sounds/Bottle.aiff", -- Visual Block (Ctrl+V)
+  c = "/System/Library/Sounds/Ping.aiff", -- Command mode (:)
+}
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+  pattern = "*",
+  callback = function(args)
+    local to_mode = args.match:sub(-1)
+    if to_mode ~= last_mode then
+      local sound = mode_sounds[to_mode]
+      if sound then
+        vim.fn.jobstart({ "afplay", sound }, { detach = true })
+      end
+      last_mode = to_mode
+    end
+  end,
+})
