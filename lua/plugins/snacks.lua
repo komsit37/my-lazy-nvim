bc264ea3dbd0413adca42e4b9e8e6920e3b42d0f
Komsit Prakobphol <pkomsit@folio-sec.com>
2025-07-11 12:41:42 +0900

    customize snack picker wider width

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,41 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        grep = {
+          layout = {
+            layout = {
+              width = 0.95,
+              height = 0.8,
+            },
+          },
+        },
+        files = {
+          layout = {
+            layout = {
+              width = 0.95,
+              height = 0.8,
+            },
+          },
+        },
+        git_status = {
+          layout = {
+            layout = {
+              width = 0.95,
+              height = 0.8,
+            },
+          },
+        },
+        git_diff = {
+          layout = {
+            layout = {
+              width = 0.95,
+              height = 0.8,
+            },
+          },
+        },
+      },
+    },
+  },
+}
