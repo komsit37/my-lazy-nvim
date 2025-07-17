6d3614054db5855bc6536711155590d262e203fe
pkomsit <pkomsit@folio-sec.com>
2025-07-17 18:04:43 +0900

    set diff highlight color

--- lua/config/highlights.lua
+++ lua/config/highlights.lua
@@ -5,10 +5,24 @@ local function set_illuminate_highlights()
   vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#333355", underline = true })
 end
 
+-- set diff highlights
+local function set_diff_highlights()
+  -- print("Setting diff highlights...")
+  vim.api.nvim_set_hl(0, "DiffDelete", { fg = "#ffffff", bg = "#7f2b37" })
+  vim.api.nvim_set_hl(0, "DiffAdd", { fg = "#ffffff", bg = "#295c2b" })
+  vim.api.nvim_set_hl(0, "DiffChange", { fg = "#ffffff", bg = "#4a3f2c" })
+  -- print("Diff highlights set")
+end
+
 -- Set once on startup
 set_illuminate_highlights()
+set_diff_highlights()
 
 -- Reapply on ColorScheme change
 vim.api.nvim_create_autocmd("ColorScheme", {
-  callback = set_illuminate_highlights,
+  pattern = "*",
+  callback = function()
+    set_illuminate_highlights()
+    set_diff_highlights()
+  end,
 })
