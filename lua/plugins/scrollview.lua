6437ba3d774fd7f593da64e4fe527bd5b2e7ed5c
pkomsit <pkomsit@folio-sec.com>
2025-07-27 05:00:54 +0700

    add rust, scrollview

---
+++ lua/plugins/scrollview.lua
@@ -0,0 +1,12 @@
+return {
+  "dstein64/nvim-scrollview",
+  -- optional: trigger loading only on certain events for performance
+  event = "VeryLazy",
+  -- optional config
+  config = function()
+    -- customize as needed
+    -- vim.g.scrollview_base = 'right'
+    -- vim.g.scrollview_cursor_symbol = '|'
+    -- see :help scrollview-configuration for more options
+  end,
+}
