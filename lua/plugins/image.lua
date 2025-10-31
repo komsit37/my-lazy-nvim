2e3db4db8c842399f83c6070073eeb05b7bc8020
pkomsit <pkomsit@folio-sec.com>
2025-10-31 09:34:28 +0900

    add image

---
+++ lua/plugins/image.lua
@@ -0,0 +1,29 @@
+-- lua/plugins/image.lua
+return {
+  "3rd/image.nvim",
+  -- load it where you want images (markdown is common)
+  ft = { "markdown", "norg", "help" },
+  opts = {
+    backend = "kitty", -- use Kitty Graphics Protocol
+    kitty_method = "unicode", -- works reliably through tmux
+    integrations = {
+      markdown = { enabled = true, clear_in_insert_mode = true },
+      neorg = { enabled = true },
+      cmp = false,
+      telescope = false,
+    },
+    -- scaling/limits (tweak to taste)
+    max_width = 60,
+    max_height = 40,
+    max_width_window_percentage = 0.6,
+    window_overlap_clear_enabled = true,
+    window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs" },
+
+    -- tmux-specific niceties
+    tmux_show_only_in_active_window = true,
+    editor_only_render_when_focused = true,
+
+    -- converter (ImageMagick CLI is the most hassle-free)
+    processor = "magick_cli",
+  },
+}
